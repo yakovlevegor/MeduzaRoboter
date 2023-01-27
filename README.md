db96d3da7e3d91a7855266b1350f0d855480b14e
yakovlevegor <egoroyakovlev@gmail.com>
2023-01-28 04:32:25 +0700

    For Meduza!

---
+++ README.md
@@ -0,0 +1,33 @@
+# Meduza Roboter
+
+A small shell script to fetch Meduza RSS feed, save the news as text files, and optionally process text to voice.
+
+### How to use it
+
+The script requires the following dependencies:
+
+`xmlstarlet python-html2text curl`
+
+Text-to-speech engine is also required: `espeak-ng`
+
+### To fetch the latest news
+
+Simply run
+
+`bash meduzaroboter.sh`
+
+If you don't want any **voice processing**, then just add an argument:
+
+`bash meduzaroboter.sh --novoice`
+
+You'll find your text articles in `meduza_articles_text` subdirectory, same with machine voice recordings: `meduza_articles_voice`
+
+### What if Meduza is blocked?
+
+Don't worry, just pass an HTTP Proxy setting to the script:
+
+`http_proxy="http://127.0.0.1:8118" bash meduzaroboter.sh`
+
+## What is Meduza?
+
+[Meduza](https://meduza.io/en) is a Russian independent news agency, that the Russian government first blocked for its honest covering of the ongoing military conflict in Ukraine, and recently it became a "non-desirable" organization in Russia, hence it is now forbidden to [even mention it in public](https://meduza.io/en/cards/life-after-undesirability). You can [**❤️  donate them**](https://support.meduza.io/en) and support their enormous work against the harsh and barbaric Russian regime. The script is based on the English version of the outlet.
---
+++ meduzaroboter.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+curl -L -o "meduza_rss_latest.txt" "https://meduza.io/rss/en/all"
+
+novoice=0
+
+for argum in $@; do
+    if [ "$argum" == "--novoice" ]; then
+        novoice=1;
+    fi
+done;
+
+if [ ! -d "meduza_articles_text" ]; then
+    mkdir "meduza_articles_text";
+fi
+
+if [[ $novoice -eq 0 ]]; then
+    if [ ! -d "meduza_articles_voice" ]; then
+        mkdir "meduza_articles_voice";
+    fi
+fi
+
+articlescount=$(xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "count(/rss/channel/item)" meduza_rss_latest.txt);
+
+articlesdone=1;
+
+while [[ $articlesdone -le $articlescount ]]; do
+    articlename=$(xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/link" meduza_rss_latest.txt | sed -E "s/https:\/\/meduza.io\/en\///" | sed -E "s/\//-/g");
+    if [ ! -f "meduza_articles_text/"$articlename".txt" ]; then
+        xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/title" meduza_rss_latest.txt > "meduza_articles_text/"$articlename".txt";
+        echo "" >> "meduza_articles_text/"$articlename".txt";
+        echo "" >> "meduza_articles_text/"$articlename".txt";
+        xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/content:encoded" meduza_rss_latest.txt | sed -E "s/&lt;p&gt;Save Meduza\!&lt;br&gt;&lt;a href='https:\/\/support.meduza.io\/en'&gt;https:\/\/support.meduza.io\/en&lt;\/a&gt;&lt;\/p&gt;//" | html2text --body-width=0 | html2text --images-to-alt --ignore-emphasis --ignore-links --body-width=0 >> "meduza_articles_text/"$articlename".txt";
+    fi
+    if [[ $novoice -eq 0 ]]; then
+        espeak -f "meduza_articles_text/"$articlename".txt" -s 260 -a 50 -g 6 -p 30 -w "meduza_articles_voice/"$articlename".wav";
+    fi
+    articlesdone=($articlesdone+1);
+done
