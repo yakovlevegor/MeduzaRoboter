ae6863a468bc34c92ec0f5ab81752ee33b9b1942
yakovlevegor <egoroyakovlev@gmail.com>
2023-01-28 13:41:39 +0700

    Don't regenerate voice records

--- meduzaroboter.sh
+++ meduzaroboter.sh
@@ -33,7 +33,9 @@ while [[ $articlesdone -le $articlescount ]]; do
         xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/content:encoded" meduza_rss_latest.txt | sed -E "s/&lt;p&gt;Save Meduza\!&lt;br&gt;&lt;a href='https:\/\/support.meduza.io\/en'&gt;https:\/\/support.meduza.io\/en&lt;\/a&gt;&lt;\/p&gt;//" | html2text --body-width=0 | html2text --images-to-alt --ignore-emphasis --ignore-links --body-width=0 >> "meduza_articles_text/"$articlename".txt";
     fi
     if [[ $novoice -eq 0 ]]; then
-        espeak -f "meduza_articles_text/"$articlename".txt" -s 260 -a 50 -g 6 -p 30 -w "meduza_articles_voice/"$articlename".wav";
+        if [ ! -f "meduza_articles_voice/"$articlename".wav" ]; then
+            espeak -f "meduza_articles_text/"$articlename".txt" -s 260 -a 50 -g 6 -p 30 -w "meduza_articles_voice/"$articlename".wav";
+        fi
     fi
     articlesdone=($articlesdone+1);
 done
