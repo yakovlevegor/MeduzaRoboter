d9b9048e8c50ff3a5215624abd1e6c9d9feb93b1
yakovlevegor <egoroyakovlev@gmail.com>
2023-01-28 14:15:46 +0700

    Sort date by year

--- meduzaroboter.sh
+++ meduzaroboter.sh
@@ -26,7 +26,7 @@ articlesdone=1;
 
 while [[ $articlesdone -le $articlescount ]]; do
     articleurlname=$(xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/link" meduza_rss_latest.txt | sed -E "s/https:\/\/meduza.io\/en\///" | sed -E "s/\//-/g");
-    articlepubdate=$(date -u --date="$(xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/pubDate" meduza_rss_latest.txt)" +%d_%m_%y);
+    articlepubdate=$(date -u --date="$(xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/pubDate" meduza_rss_latest.txt)" +%y_%m_%d);
     articlename=$articlepubdate"_"$articleurlname;
     if [ ! -f "meduza_articles_text/"$articlename".txt" ]; then
         xml sel -N atom="http://www.w3.org/2005/Atom" -N content="http://purl.org/rss/1.0/modules/content/" -t -v "/rss/channel/item[$articlesdone]/title" meduza_rss_latest.txt > "meduza_articles_text/"$articlename".txt";
